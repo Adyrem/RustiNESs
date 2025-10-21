d132920020f54e61914120515179d6d884774804
Adyrem <fanfohcd@gmail.com>
2025-10-21 21:43:58 +0200

    Reworked cpu cycles to a queue system

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,4 +1,6 @@
 
+use std::collections::VecDeque;
+
 pub struct Flags {
     flag_carry: bool,
     flag_zero: bool,
@@ -9,6 +11,7 @@ pub struct Flags {
 }
 
 pub struct Emulator {
+    queue: VecDeque<Box<dyn FnMut(&mut Self)>>,
     ram: [u8; 0x800],
     rom_header: [u8; 16],
     rom: [u8; 0x8000],
@@ -18,9 +21,6 @@ pub struct Emulator {
     pub x: u8,
     pub y: u8,
     pub halted: bool,
-    running_cycle: u8,
-    no_op_cycle: u8,
-    total_cycles: u8,
     opcode: u8,
     temp_bytes: [u8; 4], // Can be used to store values inbetween cpu cycles, not to be confused with registers
     flags: Flags
@@ -28,7 +28,9 @@ pub struct Emulator {
 
 impl Emulator {
     pub fn new(rom_all: [u8; 0x8010]) -> Self {
-        let mut ret = Self { ram: [0; 0x800],
+        let mut ret = Self { 
+            queue: VecDeque::new(),
+            ram: [0; 0x800],
             rom_header: rom_all[0..0x10].try_into().expect("Error reading ROM header"),
             rom: rom_all[0x10..0x8010].try_into().expect("Error reading ROM contents"),
             program_counter: 0,
@@ -37,9 +39,6 @@ impl Emulator {
             x: 0,
             y: 0,
             halted: false, 
-            running_cycle: 0, 
-            no_op_cycle: 0, 
-            total_cycles: 0, 
             opcode: 0, 
             temp_bytes: [0; 4],
             flags: Flags { flag_carry: false, flag_zero: false, flag_interrupt_disable: true, flag_decimal: false, flag_overflow: false, flag_negative: false }
@@ -73,118 +72,205 @@ impl Emulator {
     //Counting up the cycles will allow instructions between cycles which may come in useful in the future
     // That would not be possible if all cycles of an instruction were performed immediatly, though that would certanly make it more readable
     pub fn cpu_cycle(&mut self) {
-        //By default, the cpu cycle count is reset to 0 after an instruction
-        let mut reset = true;
 
         //Some operations take more than 1 cycle per operation, e.g. pull which takes 3 cycles for a single method call + 1 for the actual PLA instruction
-        // This will need more reworking in the future for better accuracy
-        if self.no_op_cycle > 0 {
-            self.no_op_cycle -= 1;
-            self.total_cycles += 1;
-            return;
-        }
-
-        if self.running_cycle == 0 {
+        // Each operation pushes its cycles into the queue, which are then handled one cycle at a time
+        if let Some(mut task) = self.queue.pop_front() {
+            task(self);
+        } else {
+            //If no operations are scheduled, the next opcode should be read
             self.opcode = self.read(self.program_counter);
-            reset = false;
             self.increment_pc();
-        }
-        else if self.running_cycle == 1 {
-            match self.opcode {
-                0x02 => self.halted = true,
-                0x08 => self.push_flags(),
-                0x28 => self.pull_flags(),
-                0x10 | 0x30 | 0x50 | 0x70 | 0x90 | 0xB0 | 0xD0 | 0xF0 //BPL, BMI, BVC, BVS, BCC, BCS, BNE, BEQ
-                 | 0x20 | 0x4C //JSR, JMP
-                 | 0x84..0x86 //STY, STA, STX Zero page
-                 | 0x8C..0x8E //STY, STA, STX Absolute
-                 | 0xA5 //LDA Zero page
-                 | 0xAD //LDA Absolute
-                 => { self.temp_bytes[0] = self.read(self.program_counter); reset = false; self.increment_pc();}, 
-                0x18 => { self.flags.flag_carry = false } //CLC
-                0x38 => { self.flags.flag_carry = true } //SEC
-                0x58 => { self.flags.flag_interrupt_disable = false } //CLI
-                0x78 => { self.flags.flag_interrupt_disable = true } //SEI
-                0xB8 => { self.flags.flag_overflow = true } //CLV
-                0xD8 => { self.flags.flag_decimal = false } //CLD
-                0xF8 => { self.flags.flag_decimal = true } //SED
-                0xEA => { } //NOP
-                0x48 => { self.push(self.a); } //PHA
-                0x60 => { self.temp_bytes[0] = self.pull(); reset = false;} //RTS
-                0x68 => { self.a = self.pull();} //PLA
-                0x88 => { self.y -= 1; self.set_flags(self.x);}, //DEY
-                0x8A => { self.a = self.x; self.set_flags(self.a);}, //TXA
-                0x98 => { self.a = self.y; self.set_flags(self.a);}, //TYA
-                0x9A => { self.stack_pointer = self.x }, //TXS
-                0xA0 => { self.y = self.read(self.program_counter); self.increment_pc();}, //LDY Immediate
-                0xA2 => { self.x = self.read(self.program_counter); self.increment_pc();}, //LDX Immediate
-                0xA9 => { self.a = self.read(self.program_counter); self.set_flags(self.a); self.increment_pc();}, //LDA Immediate
-                0xA8 => { self.y = self.a; self.set_flags(self.y);}, //TAY
-                0xAA => { self.x = self.a; self.set_flags(self.x);}, //TAX
-                0xBA => { self.x = self.stack_pointer; self.set_flags(self.x);}, //TSX
-                0xC8 => { self.y += 1; self.set_flags(self.y);}, //INY
-                0xCA => { self.x -= 1; self.set_flags(self.x);}, //DEX
-                0xE8 => { self.x += 1; self.set_flags(self.x);}, //INX
-                _ => {self.missing_opcode();}
-            };
-
-        }
-        else if self.running_cycle == 2 {
-            match self.opcode {
-                0x10 => { if !self.flags.flag_negative { self.take_branch(self.temp_bytes[0]) } ; }, //BPL
-                0x20 => { self.temp_bytes[1] = self.read(self.program_counter); reset = false; }, //JSR 
-                0x30 => { if self.flags.flag_negative { self.take_branch(self.temp_bytes[0]) } ; }, //BMI
-                0x4C => { self.temp_bytes[1] = self.read(self.program_counter); self.program_counter = self.temp_bytes_to_little_endian() }, //JSR 
-                0x60 => { self.temp_bytes[1] = self.pull(); reset = false;} //RTS
-                0x84 => { self.write(self.temp_bytes[0] as u16, self.y); }, //STY Zero page
-                0x85 => { self.write(self.temp_bytes[0] as u16, self.a); }, //STA Zero page
-                0x86 => { self.write(self.temp_bytes[0] as u16, self.x); }, //STX Zero page
-                0x8C..0x8E //STY, STA, STX Absolute
-                | 0xAD //LDA Absolute
-                 => { self.temp_bytes[1] = self.read(self.program_counter); reset = false; self.increment_pc(); }, 
-                0xA5 => { self.a = self.read(self.temp_bytes[0] as u16); self.set_flags(self.a) }, //LDA Zero page
-                0xD0 => { if !self.flags.flag_zero { self.take_branch(self.temp_bytes[0]) } ; }, //BNE
-                0xF0 => { if self.flags.flag_zero { self.take_branch(self.temp_bytes[0]) } ; }, //BEQ
-                _ => {self.missing_opcode();}
-            };
-        }
-        else if self.running_cycle == 3 {
-            match self.opcode {
-                0x20 => { self.push((self.program_counter/256) as u8); reset = false; } //JSR
-                0x60 => { self.program_counter = self.temp_bytes_to_little_endian(); self.increment_pc();} //RTS
-                0x8C => { self.write(self.temp_bytes_to_little_endian(), self.y); }, //STY Absolute 
-                0x8D => { self.write(self.temp_bytes_to_little_endian(), self.a); }, //STA Absolute
-                0x8E => { self.write(self.temp_bytes_to_little_endian(), self.x); }, //STX Absolute
-                0xAD => { self.a = self.read(self.temp_bytes_to_little_endian()); self.set_flags(self.a)}, //LDA Absolute
-                _ => {self.missing_opcode();}
-            };
-        }
-        else if self.running_cycle == 4 {
-            match self.opcode {
-                0x20 => { self.push(self.program_counter as u8); reset = false; } //JSR
-                _ => {self.missing_opcode();}
-            };
-        }
-        else if self.running_cycle == 5 {
-            match self.opcode {
-                0x20 => { self.program_counter = self.temp_bytes_to_little_endian()} //JSR
-                _ => {self.missing_opcode();}
-            };
+            self.queue_Opcode();
         }
 
         //TODO this should be expanded to all relevant values and maybe also logged to something better than console
         println!("opcode {0:x} | tempBytes {1:x} {2:x} | pc 0x{3:x} | a 0x{4:x}",
          self.opcode, self.temp_bytes[0], self.temp_bytes[1], self.program_counter, self.a);
-
-        self.running_cycle += 1;
-        self.total_cycles += 1;
-
-        if reset {
-            self.running_cycle = 0; 
-            self.no_op_cycle = 0;
-            self.total_cycles = 0;
-        }
     }
+
+    //No completely sure how this actually works, thanks chatgpt
+    fn add_task<F>(&mut self, f: F)
+    where
+        F: FnMut(&mut Self) + 'static,
+    {
+        self.queue.push_back(Box::new(f));
+    }
+
+    fn add_task_front<F>(&mut self, f: F)
+    where
+        F: FnMut(&mut Self) + 'static,
+    {
+        self.queue.push_front(Box::new(f));
+    }
+
+    //This can be called when a function needs to take up multiple cycles but its not clear where the cycles come from
+    // Should only be used if no other solution is clear
+    fn add_NOP(&mut self)
+    {
+        self.add_task_front(|s| {});
+    }
+
+    fn queue_Opcode(&mut self) {
+        match self.opcode {
+                0x02 => //HLT
+                    self.add_task(|s| s.halted = true),
+                0x08 => //PHP
+                    self.add_task(|s| s.push_flags()),
+                0x28 => //PLP
+                    self.add_task(|s| s.pull_flags()),
+                0x10 => //BPL
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { if !s.flags.flag_negative { s.take_branch(s.temp_bytes[0]) } } );
+                },
+                0x20 => //JSR
+                { 
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| s.temp_bytes[1] = s.read(s.program_counter));
+                    self.add_task(|s| s.push((s.program_counter/256) as u8));
+                    self.add_task(|s| s.push(s.program_counter as u8));
+                    self.add_task(|s| s.program_counter = s.temp_bytes_to_little_endian());
+                },
+                0x30 => //BMI
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { if s.flags.flag_negative { s.take_branch(s.temp_bytes[0]) } } );
+                },
+                0x50 => //BVC
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                },
+                0x70 => //BVS 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                },
+                0x90 => //BCC 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                },
+                0xB0 => //BCS 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                },
+                0xD0 => //BNE 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { if !s.flags.flag_zero { s.take_branch(s.temp_bytes[0]) } } );
+                },
+                0xF0 => //BEQ 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { if s.flags.flag_zero { s.take_branch(s.temp_bytes[0]) } } );
+
+                },
+                0x4C => //JMP 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.temp_bytes[1] = s.read(s.program_counter); s.program_counter = s.temp_bytes_to_little_endian() } );
+                },
+                0x84 => //STY Zero page 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.write(s.temp_bytes[0] as u16, s.y); } );
+                },
+                0x85 => //STA Zero page 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.write(s.temp_bytes[0] as u16, s.a); } );
+                },
+                0x86 => //STX Zero page 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.write(s.temp_bytes[0] as u16, s.x); } );
+                },
+                0x8C => //STY Absolute 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.temp_bytes[1] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.write(s.temp_bytes_to_little_endian(), s.y); } );
+                },
+                0x8D => //STA Absolute 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.temp_bytes[1] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.write(s.temp_bytes_to_little_endian(), s.a); } );
+                },
+                0x8E => //STX Absolute 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.temp_bytes[1] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.write(s.temp_bytes_to_little_endian(), s.x); } );
+                },
+                0xA5 => //LDA Zero page 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.a = s.read(s.temp_bytes[0] as u16); s.set_flags(s.a) } );
+                },
+                0xAD => //LDA Absolute 
+                {
+                    self.add_task(|s| { s.temp_bytes[0] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.temp_bytes[1] = s.read(s.program_counter); s.increment_pc() } );
+                    self.add_task(|s| { s.a = s.read(s.temp_bytes_to_little_endian()); s.set_flags(s.a) } );
+                },
+                0x18 => //CLC
+                    self.add_task(|s| s.flags.flag_carry = false), 
+                0x38 => //SEC
+                    self.add_task(|s| s.flags.flag_carry = true ), 
+                0x58 => //CLI
+                    self.add_task(|s| s.flags.flag_interrupt_disable = false ), 
+                0x78 => //SEI
+                    self.add_task(|s| s.flags.flag_interrupt_disable = true ), 
+                0xB8 => //CLV
+                    self.add_task(|s| s.flags.flag_overflow = true ), 
+                0xD8 => //CLD
+                    self.add_task(|s| s.flags.flag_decimal = false ), 
+                0xF8 => //SED
+                    self.add_task(|s| s.flags.flag_decimal = true ), 
+                0xEA => //NOP
+                    self.add_task(|s| {}), 
+                0x48 => //PHA
+                   self.add_task(|s| s.push(s.a)), 
+                0x60 => //RTS
+                {
+                    self.add_task(|s| s.temp_bytes[0] = s.pull());
+                    self.add_task(|s| s.temp_bytes[1] = s.pull());
+                    self.add_task(|s| { s.program_counter = s.temp_bytes_to_little_endian(); s.increment_pc() } );
+                },
+                0x68 => //PLA
+                    self.add_task(|s| s.a = s.pull()),
+                0x88 => //DEY
+                    self.add_task(|s| { s.y -= 1; s.set_flags(s.x) } ), 
+                0x8A => //TXA
+                    self.add_task(|s| { s.a = s.x; s.set_flags(s.a) }), 
+                0x98 => //TYA
+                  self.add_task(|s| { s.a = s.y; s.set_flags(s.a) } ), 
+                0x9A => //TXS
+                    self.add_task(|s| s.stack_pointer = s.x ), 
+                0xA0 => //LDY Immediate
+                    self.add_task(|s| { s.y = s.read(s.program_counter); s.increment_pc() } ),
+                0xA2 => //LDX Immediate
+                    self.add_task(|s| { s.x = s.read(s.program_counter); s.increment_pc() } ),
+                0xA9 => //LDA Immediate
+                    self.add_task(|s| { s.a = s.read(s.program_counter); s.set_flags(s.a); s.increment_pc() } ),
+                0xA8 => //TAY
+                    self.add_task(|s| { s.y = s.a; s.set_flags(s.y) } ), 
+                0xAA => //TAX
+                    self.add_task(|s| { s.x = s.a; s.set_flags(s.x) } ), 
+                0xBA => //TSX
+                    self.add_task(|s| { s.x = s.stack_pointer; s.set_flags(s.x) } ), 
+                0xC8 => //INY
+                    self.add_task(|s| { s.y += 1; s.set_flags(s.y) } ), 
+                0xCA => //DEX
+                    self.add_task(|s| { s.x -= 1; s.set_flags(s.x) } ), 
+                0xE8 => //INX
+                    self.add_task(|s| { s.x += 1; s.set_flags(s.x) } ), 
+                _ => //Not implemented 
+                    self.add_task(|s| s.missing_opcode())
+            };
+
+    }
+    
 
     fn temp_bytes_to_little_endian(&self) -> u16 {
         return self.temp_bytes[1] as u16 * 0x100 + self.temp_bytes[0] as u16;
@@ -204,7 +290,7 @@ impl Emulator {
         if signed_val > 127 {
             signed_val -= 256;
             self.program_counter += signed_val as u16;
-            self.no_op_cycle += 1;
+            self.add_NOP();
         }
         else
         {
@@ -215,13 +301,14 @@ impl Emulator {
     fn push(&mut self, val: u8) {
         self.write(0x100 + self.stack_pointer as u16, val);
         self.stack_pointer -= 1;
-        self.no_op_cycle += 1;
+        self.add_NOP();
     }
 
     fn pull(&mut self) -> u8 {
         self.stack_pointer += 1;
         let ret_val = self.read(0x100 + self.stack_pointer as u16);
-        self.no_op_cycle += 2;
+        self.add_NOP();
+        self.add_NOP();
         return ret_val;
     }
 
